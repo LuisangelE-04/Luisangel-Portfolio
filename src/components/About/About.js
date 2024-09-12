a5e02b18dcb53075b203adf8da874e25c2d06077
Luisangel Esparza <luisangel@Luisangels-MacBook-Air.local>
2024-09-11 19:52:39 -0500

    Updated layout on about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,8 +3,11 @@ import './About.css'
 const About = () => {
     return (
         <div className="about">
-            <h1>Luisangel Esparza</h1>
-            <h2>Computer Science Student</h2>
+            <div className="title">
+            <h1>Luisangel</h1>
+            <h1>Esparza</h1>
+            </div>
+            <h2>Computer Science Student @ University of Houston</h2>
             <div className="about-text">
                 <p>
                     Hello, my name is Luisanel! I am an aspiring developer attending the University of Houston!
