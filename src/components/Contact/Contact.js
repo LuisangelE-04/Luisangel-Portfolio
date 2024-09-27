3877eea3bd248de80c39100c59b01be08266c475
Luisangel Esparza <luisangel@Luisangels-MacBook-Air.local>
2024-09-27 00:45:30 -0500

    add favicon sizes and add contact page

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,11 @@
+import './Contact.css'
+
+const Contact = () => {
+    return (
+        <div className="contact">
+            <h1>Contact Me!</h1>
+        </div>
+    );
+}
+
+export default Contact
